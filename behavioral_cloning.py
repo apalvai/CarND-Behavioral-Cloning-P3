77d20b7ca667349b3c35eba2442de2be709eb50f
apalvai <anoopreddy06@gmail.com>
2017-03-19 02:09:30 -0700

    Used nvidia model to drive the car.

--- behavioral_cloning.py
+++ behavioral_cloning.py
@@ -3,9 +3,10 @@ import numpy as np
 import cv2
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from keras.callbacks import EarlyStopping
 
 # fetch data
 print('reading data...')
@@ -21,20 +22,60 @@ lines = lines[1:]
 images = []
 measurements = []
 for line in lines:
-    # read image
-    image_path = line[0]
-    filename = image_path.split('/')[-1]
-    current_path = './data/IMG/' + filename
-    image = cv2.imread(current_path)
+    
+    # read center image
+    center_image_filename = line[0].split('/')[-1]
+    center_image_path = './data/IMG/' + center_image_filename
+    image = cv2.imread(center_image_path)
+#    image = image.reshape(image.shape + (1,))
     images.append(image)
-    flipped_image = np.fliplr(image)
-    images.append(flipped_image)
     
     # read measurement
     measurement = float(line[3])
     measurements.append(measurement)
+    
+    # flipped center image and it's measurement
+    flipped_image = np.fliplr(image)
+    images.append(flipped_image)
     measurement_flipped = -measurement
     measurements.append(measurement_flipped)
+    
+    # correction
+    correction = 0.05
+    
+    # read left image
+    left_image_filename = line[1].split('/')[-1]
+    left_image_path = './data/IMG/' + left_image_filename
+    left_image = cv2.imread(left_image_path)
+#    left_image = left_image.reshape(left_image.shape + (1,))
+    images.append(left_image)
+    
+    # read left measurement
+    left_measurement = measurement + correction
+    measurements.append(left_measurement)
+    
+    # flipped left image and it's measurement
+    flipped_left_image = np.fliplr(left_image)
+    images.append(flipped_left_image)
+    left_measurement_flipped = -left_measurement
+    measurements.append(left_measurement_flipped)
+    
+    # read right image
+    right_image_filename = line[2].split('/')[-1]
+    right_image_path = './data/IMG/' + right_image_filename
+    right_image = cv2.imread(right_image_path)
+#    right_image = right_image.reshape(right_image.shape + (1,))
+    images.append(right_image)
+    
+    # read right measurement
+    right_measurement = measurement - correction
+    measurements.append(right_measurement)
+    
+    # flipped right image and it's measurement
+    flipped_right_image = np.fliplr(right_image)
+    images.append(flipped_right_image)
+    right_measurement_flipped = -right_measurement
+    measurements.append(right_measurement_flipped)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -46,19 +87,25 @@ input_shape = X_train[0].shape
 
 # model
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=input_shape))
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
+model.add(Cropping2D(cropping=((60, 20), (0, 0)), input_shape=input_shape))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(90, 320, 3))) #Image normalization
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(36, 3, 3, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(48, 3, 3, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
-model.add(Dense(128))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 # training & validation
+print('training...')
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-model.fit(X_train, y_train, verbose=1, batch_size=32, validation_split=0.2, shuffle=True)
+#early_stopping = EarlyStopping(monitor='val_loss', patience=2)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3) # callbacks=[early_stopping]
 
 # save model
+print('saving model...')
 model.save('model.h5')
